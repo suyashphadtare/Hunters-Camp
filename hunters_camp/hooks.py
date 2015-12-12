cde9c647240f40d77e7b4ee586b2c4282c6ccf27
suyashphadtare <suyash.p@indictranstech.com>
2015-12-12 10:31:54 +0530

    [Fix] Adding role in fixtures

--- hunters_camp/hooks.py
+++ hunters_camp/hooks.py
@@ -35,7 +35,7 @@ app_include_js = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDO87KWhu8luH
 # Generators
 # ----------
 
-fixtures = ["Custom Field"]
+fixtures = ["Custom Field", "Role"]
 # automatically create page for each record of this doctype
 # website_generators = ["Web Page"]
 
