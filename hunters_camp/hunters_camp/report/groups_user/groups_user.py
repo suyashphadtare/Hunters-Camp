5a87f71e390701e131866b40c45ac1316728f529
pawaranand <pawaranand00@gmail.com>
2016-01-15 19:16:23 +0530

    [Fix]

--- hunters_camp/hunters_camp/report/groups_user/groups_user.py
+++ hunters_camp/hunters_camp/report/groups_user/groups_user.py
@@ -19,7 +19,9 @@ def execute(filters=None):
 
 def get_result(filters):
 	if filters.get("group"):
-		return frappe.db.sql("""SELECT `tabGroup`.group_title,
+		return frappe.db.sql("""SELECT 
+			`tabGroup`.name,
+			`tabGroup`.group_title,
 			`tabUser`.first_name,
 			`tabUser`.last_name,
 			`tabUser`.mobile_no,
@@ -35,7 +37,9 @@ def get_result(filters):
 			WHERE `tabGroup User`.group_id ='{0}'""".format(filters.get("group")),as_list=1)
 
 def get_columns():
-	return [_("Group Title") + ":Data:200",
+	return [
+			_("Group Id") + ":Link/Group:200",
+			_("Group Title") + ":Data:200",
 			_("First Name") + ":Data:100",
 			_("Last Name") + ":Data:100", 
 			_("Mobile No") + ":Data:100",
